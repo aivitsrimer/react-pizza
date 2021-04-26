13104996e837b99d57d9ccf9006813b9a370f78f
Vits-Rimer Anna <urazaeva.anna@gmail.com>
2021-04-26 17:22:18 +0300

    Changed connect to hook useSelector, useDispatch

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
 import React from 'react';
 import axios from 'axios';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart } from "./pages";
 import { Route } from "react-router-dom";
 import { setPizzas } from './redux/actions/pizzas';
 
+function App() {
+  const dispatch = useDispatch();
+  const { items } = useSelector(({ pizzas, filters}) => {
+    return {
+      items: pizzas.items,
+      sortBy: filters.sortBy,
+    }
+  });
 
-// function App() {  
-//   React.useEffect(() => {
-//     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-//       setPizzas(data.pizzas);
-//     });
-//   }, []);
-//   return (
-//   );
-// }
-
-class App extends React.Component {
-  componentDidMount() {
+  React.useEffect(() => {
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      this.props.setPizzas(data.pizzas);
+      dispatch(setPizzas(data.pizzas));
     });
-  }
+  }, []);
 
-  render() {
-    return(
-      <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Route exact path="/" render={() => <Home items={this.props.items} />} />
-        <Route exact path="/cart" component={Cart} />
-      </div>
+  return (
+    <div className="wrapper">
+    <Header />
+    <div className="content">
+      <Route exact path="/" render={() => <Home items={items} />} />
+      <Route exact path="/cart" component={Cart} />
     </div>
-    );
-  }
+  </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-  }
-}
+export default App;
+
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.pizzas.items,
+//   }
+// }
 
 // const mapDispatchToProps = (dispatch) => {
 //   return {
@@ -50,8 +47,8 @@ const mapStateToProps = (state) => {
 //   };
 // }
 
-const mapDispatchToProps = {
-  setPizzas,
-}
+// const mapDispatchToProps = {
+//   setPizzas,
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
